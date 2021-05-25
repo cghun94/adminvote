bcc660d8f744b2efadc618eaa3293f0455643998
cghun94 <cghun94@gmail.com>
2021-05-25 05:51:04 +0000

    210525 routes folder add

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const createError = require('http-errors');
 const express = require('express');
+const app = express();
 const logger = require('morgan');
 
 //라우터 설정
-var indexRouter = require('./routes/index')
+var indexRouter = require('./routes/index');
+
+
+
+
+
+
+app.use('/',indexRouter);
+
+module.exports = app;
