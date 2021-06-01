77b94e7dd07e9f3bf0152e03c1e3847c7a152b79
cghun94 <cghun94@gmail.com>
2021-06-01 10:35:17 +0000

    210601 jwt토큰 처리

---
+++ routes/jwttoken.js
@@ -0,0 +1,47 @@
+require('dotenv').config({path : '/root/admin/.env'});
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    accessToken: (req, res) => {
+      const token = req.headers.authorization;
+  
+      if (token) {
+        const token_body = token.split(' ')[1];
+        jwt.verify(token_body, process.env.ACCESS_SECRET, (err, decoded) => {
+          if (err) {
+            res.status(400).send('invalid access token');
+          } else {
+            res.status(200).json({
+              id: decoded.id
+            });
+          }
+        });
+      } else {
+        res.status(400).send('invalid access token');
+      }
+    },
+    refreshToken: (req, res) => {
+      const refresh = req.cookies.refreshToken;
+      if (refresh) {
+        if (refresh === 'invalidtoken') {
+          res.status(400).send('invalid refresh token, please log in again');
+        } else {
+          jwt.verify(refresh, process.env.REFRESH_SECRET, (err, decoded) => {
+            if (err) {
+              res.status(400).send('invalid refresh token');
+            } else {
+              const access_token = jwt.sign({}, process.env.ACCESS_SECRET);
+              res.status(200).json({
+                accessToken: access_token,
+                userInfo: {
+                  id: decoded.id,
+                },
+              });
+            }
+          });
+        }
+      } else {
+        res.status(400).send('refresh token not provided');
+      }
+    },
+  };
